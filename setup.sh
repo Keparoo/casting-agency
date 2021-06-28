92b346b3a1a4db8185dce5bd0e4ef14d91430f61
Kep <KepCreations@gmail.com>
2021-06-28 13:30:28 -0400

    update setup & gitignore

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 FLASK_APP=app.py
 FLASK_ENV=development
+FLASK_DEBUG=True
+ENV='development'
+
+AUTH0_CLIENT_ID = ''
+AUTH0_CLIENT_SECRET =''
+AUTH0_DOMAIN =''
+
 alias err98="ps -fA | grep python"
 
 DATABASE_URL=postgresql+psycopg2://kep:password@127.0.0.1:5432/casting
