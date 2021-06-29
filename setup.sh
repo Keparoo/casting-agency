862607d60796a3df38b1b26e882ba80f1366dd25
Kep <KepCreations@gmail.com>
2021-06-29 13:02:27 -0400

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
-FLASK_APP=app.py
-FLASK_ENV=development
-FLASK_DEBUG=True
-ENV='development'
+export FLASK_APP=app.py
+export FLASK_ENV=development
+export FLASK_DEBUG=True
+export ENV='development'
 
-AUTH0_CLIENT_ID = ''
-AUTH0_CLIENT_SECRET =''
-AUTH0_DOMAIN =''
+export AUTH0_CLIENT_ID = ''
+export AUTH0_CLIENT_SECRET =''
+export AUTH0_DOMAIN =''
 
 alias err98="ps -fA | grep python"
 
-DATABASE_URL=postgresql+psycopg2://kep:password@127.0.0.1:5432/casting
+export DATABASE_URL=postgresql+psycopg2://kep:password@127.0.0.1:5432/casting
