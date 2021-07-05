d94bf2610a1eed7fe2820af738631320c717af7f
Kep <KepCreations@gmail.com>
2021-07-05 15:53:48 -0400

    fix date in manage.py

--- manage.py
+++ manage.py
@@ -13,8 +13,8 @@ manager.add_command('db', MigrateCommand)
 
 @manager.command
 def seed():
-    Movie(title='Call Me By Your Name', release_date='2020').insert()
-    Movie(title='The Boys in the Band', release_date='2020').insert()
+    Movie(title='Call Me By Your Name', release_date='1/19/2018').insert()
+    Movie(title='The Boys in the Band', release_date='9/30/2020').insert()
 
     Actor(name='Timothee Chalamet', age=26, gender='male').insert()
     Actor(name='Zachary Quinto', age=44, gender='male').insert()
