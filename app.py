540235cc2db2c4577cce6b813bad3094e670eb2e
Kep <KepCreations@gmail.com>
2021-07-09 16:59:22 -0400

    fix AUTH0_AUDIENCE error

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ AUTH0_CALLBACK_URL = os.environ['AUTH0_CALLBACK_URL']
 AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']
 AUTH0_CLIENT_ID = os.environ['AUTH0_CLIENT_ID']
 AUTH0_CLIENT_SECRET = os.environ['AUTH0_CLIENT_SECRET']
-AUTH0_AUDIENCE = os.environ['API_AUDIENCE']
+AUTH0_AUDIENCE = os.environ['AUTH0_AUDIENCE']
 AUTH0_BASE_URL='https://' + AUTH0_DOMAIN
 
 # create and configure the Flask app
