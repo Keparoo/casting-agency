b0dc1eba653d7cbdd68d628f0e9b840bb1ae87b7
Kep <KepCreations@gmail.com>
2021-07-09 14:24:44 -0400

    update auth.py comments

--- auth.py
+++ auth.py
@@ -16,8 +16,11 @@ API_AUDIENCE = 'casting-agency'
 # Implement JWT authorization
 #----------------------------------------------------------------------------#
 
-# A standardized way to communicate auth failure modes
 class AuthError(Exception):
+    '''
+    AuthError Exception
+    A standardized way to communicate auth failure modes
+    '''
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
